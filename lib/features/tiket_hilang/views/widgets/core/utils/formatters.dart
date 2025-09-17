f78e19009923e95ec9e3cc05f31e54daceb484ab
Muhammad Kasim <muhammad.kasimacing@gmail.com>
2025-09-17 10:49:21 +0800

    Update Fitur settings

--- lib/features/tiket_hilang/views/widgets/core/utils/formatters.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class AppFormatters {
-  static final currency = NumberFormat.currency(
-    locale: 'id',
-    symbol: '',
-    decimalDigits: 2,
-  );
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'features/tiket_hilang/views/tiket_hilang_view.dart'; 
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Aplikasi Parkir',
-//       debugShowCheckedModeBanner: false, // Menghilangkan banner "Debug"
-//       theme: ThemeData(
-//         brightness: Brightness.dark, // Mengatur tema gelap secara global
-//         fontFamily: 'Poppins', // Contoh jika ingin menggunakan font kustom
-//       ),
-//       // 2. Ganti halaman utama dengan LostTicketScreen
-//       home: const LostTicketView(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'features/settings/views/settings_views.dart'; 
 
